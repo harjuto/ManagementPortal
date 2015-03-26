53000ab1734950e0dfba6db3ab4a2846eb9ade97
Tomi Harju <tomiharju@Tomis-MacBook-Air.local>
2015-03-26 18:48:22 +0200

    Initial commit, forked lauantailanit

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,50 +1,9 @@
 angular.module('app', [
   'ngRoute',
-  'ngStorage',
   'areas.home',
-  'areas.info',
-  'areas.profile',
-  'directives.navigation',
-  'services.security',
-  'services.config',
-  'services.mapServiceModule'
 ])
 
-.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.interceptors.push('TokenInterceptor');
-  }])
-  .run(['$rootScope', '$location', '$window', 'AuthenticationService', function($rootScope, $location, $window, AuthenticationService) {
-    FastClick.attach(document.body);
-    $rootScope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
-      //redirect only if both isAuthenticated is false and no token is set
-      if (nextRoute !== undefined && nextRoute.access !== undefined && nextRoute.access.requiredLogin && !AuthenticationService.isAuthenticated()) {
-        $location.path("/home");
-      }
-    });
-  }])
-  .controller('MainCtrl', ['$scope', 'AuthenticationService', 'UserService',
-    function($scope, AuthenticationService, UserService) {
 
-      this.login = function(user) {
-        console.log(user);
-        UserService.login(user).then(function(result) {
-            $('#loginModal').hide();
-          },
-          function(reason) {
-            // add something like "coudnt log in here"
-            alert(reason);
-          });
-      };
 
-      this.signup = function(user) {
-        UserService.signup(user).then(function(result) {
-          //handle success
-          alert("YES BONUS");
-        }, function(reason) {
-          // handle error'
-          alert(reason);
-        });
-      };
-
-    }
-  ]);
+.controller('MainCtrl', ['$scope', function($scope) {}
+]);
--- client/src/app/home/home.js
+++ client/src/app/home/home.js
@@ -1,27 +1,16 @@
-angular.module('areas.home', ['uiGmapgoogle-maps', 'ngMessages', 'home.directives'])
+angular.module('areas.home', [])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    name: 'home',
-    templateUrl: '/app/home/home.tpl.html',
-    controller: 'HomeCtrl',
-    controllerAs: 'home'
-  });
-}])
-
-.config(
-  ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProvider) {
-    GoogleMapApiProvider.configure({
-      v: '3.17',
-      libraries: 'places',
+    $routeProvider.when('/', {
+      name: 'home',
+      templateUrl: '/app/home/home.tpl.html',
+      controller: 'HomeCtrl',
+      controllerAs: 'home'
     });
   }])
 
-.controller('HomeCtrl', ['$scope', 'MapService', function($scope, MapService) {
-  var self = this;
-  MapService.mapPromise.then(function(map) {
-    self.map = map;
-  });
+.controller('HomeCtrl', ['$scope', function($scope) {
+    var self = this;
 
 
-}]);
+  }]);
