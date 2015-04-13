b68e010581b867107aa989da2cb56507dc104efb
Tomi Harju <tomiharju@Tomis-MacBook-Air.local>
2015-04-13 17:57:00 +0300

    Updated json web token version

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,2 +1,15 @@
-#rm -rf ManagementPortal
-#git clone https://github.com/harjuto/ManagementPortal.git
+#!/bin/bash
+cd /var/portal
+rm -rf ManagementPortal
+git clone https://github.com/harjuto/ManagementPortal.git
+echo 'Cloning latest version...'
+cd ManagementPortal
+cd server
+npm install
+cd ../client
+npm install
+bower install
+gulp
+cd ../server
+echo 'Starting server...'
+node server.js
