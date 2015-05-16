7117d746b78a3dfa951a1b8ba4357780454e91ec
Tomi Harju <tomiharju@Tomis-MacBook-Air.local>
2015-05-16 13:06:02 +0300

    Cleaning up server.js

--- server/lib/routes/app.js
+++ server/lib/routes/app.js
@@ -1,7 +1,6 @@
 var config = require('../config');
 var path = require('path');
 var express = require('express');
-var api = require("./api.js");
 var staticResources = require('./static.js');
 // app/routes.js
 module.exports = function(app) {
@@ -15,9 +14,6 @@ module.exports = function(app) {
 
   //Static resources
   app.use("/static", staticResources);
-
-  //API
-  app.use("/api", api);
 
   //Render index if any other url
   app.use(function(req, res) {
--- server/server.js
+++ server/server.js
@@ -1,20 +1,11 @@
 // Required Modules
 var express = require("express");
-var morgan = require("morgan");
-var bodyParser = require("body-parser");
-var jwt = require("jsonwebtoken");
 var config = require('./lib/config');
 var path = require("path");
-var cookieParser = require('cookie-parser');
 var app = express();
 var port = process.env.PORT || 1337;
 
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-app.use(morgan("dev"));
+
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -29,5 +20,5 @@ require('./lib/routes/app.js')(app);
 
 // launch ======================================================================
 app.listen(port, function() {
-  console.log('Express server listening on port ' + config.get('port'));
+  console.log('Express server listening on port ' + port);
 });
